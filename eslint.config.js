08b21320975e0ca1f5bc201fe62a19aa96592feb
simakovand <82641842+simakovand@users.noreply.github.com>
2024-11-15 02:11:02 +0300

    work eslint

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'react': react,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react/jsx-max-props-per-line': ['error', { maximum: 1 }],
+      'indent': ['error', 'tab', { "SwitchCase": 1 }],
+      'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+    },
+    settings: {
+      "tabWidth": 2 // Для поддержки табов с шириной 4
+    }
+  },
+)
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+	const col = {col: 'red'}
+	return (
+		<div style={{...col, color: 'red'}} >
+										zoloopa
+		</div>
+	)
+}
+
+export default App
